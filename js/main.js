cff98e4e3957a5470c5cf78f3296a1fa9023a69c
Ekaterina <kusa2706@gmail.com>
2021-09-27 21:24:29 +0300

    добавляет работу функции

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 // Вычисляет целое число//
 
 let getRandomIntNumber = function (min, max) {
+  if (min >= max || min < 0) {
+    throw new Error ('Неверный формат числа');
+  }
   return Math.floor(Math.random() * (max - min) + min);
 };
-console.log (getRandomIntNumber (10, 50));
+
+(getRandomIntNumber (10, 50));
 
 // Вычисляет дробное число//
 
 let getRandomNumber = function (min, max, num) {
+  if (min >= max || min < 0) {
+    throw new Error ('Неверный формат числа');
+  }
   return (Math.random() * (max - min) + min).toFixed(num);
 };
 
-console.log (getRandomNumber (10, 30, 3));
+(getRandomNumber (10, 30, 3));
